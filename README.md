63c0f2ddc9f06fd7df9e3880244ae32c88cbf67d
Joseph C <neckro@gmail.com>
2013-01-13 20:49:47 -0600

    Major rework of marquee.py into a proper module; add README for GitHub

---
+++ README.md
@@ -0,0 +1,44 @@
+# Marquee scroller
+
+This is a silly thing I wrote for Raspberry Pi and Arduino.  There are two
+parts, an Arduino sketch and a Python module.
+
+## led_marquee.ino
+
+Requires [LedControl library](http://playground.arduino.cc/Main/LedControl).
+
+A simple Arduino sketch that reads data from the serial port and scrolls
+columns on an attached LED matrix using the LedControl library for Maxim LED
+controllers. It's currently hardcoded for an 8x8 pixel display but could easily
+be modified for other controllers or sizes.
+
+The data format is currently pairs of (lowercase) hexadecimal digits.  If any
+invalid character is received, it is ignored and value reading is aborted
+(for example 'a 4 b 8 7' would do nothing because of the spaces).  This is to
+avoid possible desynchronization.
+
+## marquee.py
+
+This Python module writes data to the serial port for use with led_marquee.ino.
+Intended for use with the Raspberry Pi but should work anywhere.
+
+Parameter 'serial' is a dict of parameters for a pySerial object.  If it is set
+to None it will output to stdout instead, for testing/amusement purposes.
+If the module is run directly it reads files specified on the command line
+or text piped via stdin.
+
+I've had a tricky time finding TrueType pixel fonts that look good at 8px
+height without being so wide that they take forever to scroll.  Here are the
+ones I found that worked reasonably well:
+
+* [TinyUnicode](http://www.dafont.com/tinyunicode.font)
+* [Uni-05](http://www.dafont.com/uni-05-x.font)
+* [Pixel Millennium](http://www.dafont.com/pixel-millennium.font)
+
+Requires the Python Imaging Library (PIL) with FreeType support compiled.
+
+---
+Copyright © 2013 neckro@gmail.com
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
--- marquee.py
+++ marquee.py
@@ -1,83 +1,132 @@
 #!/usr/bin/env python
 
-from serial import Serial
+# Marquee generator, neckro@gmail.com 2013.
+# See README.md for details.
+
+from PIL import Image, ImageFont, ImageDraw
 from time import sleep
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
-from fileinput import input
-
-led = Serial("/dev/ttyUSB0", 9600)
-delay = 0.035
-font = "TinyUnicode.ttf"
-# need to wait for arduino to reset
-sleep(1.5)
-space_width = 3
+import string
 
 
-def led_output(n):
-    # zero padded lowercase 8-bit hex
-    h = ('%02X' % (n)).lower()
-    led.write(h)
-    #print(h)
-    sleep(delay)
+def marquee_stdin():
+    from fileinput import input
+    marquee = Marquee(**marquee_settings)
+    for line in input():
+        marquee.write_line(line, u'\u00b6')  # prefix with pilcrow
+
+serial_settings = {'port': '/dev/ttyUSB0', 'baudrate': 9600}
+
+fonts = {
+    'tinyunicode': {
+        'file': 'TinyUnicode.ttf',
+        'size': 16,
+        'offset': -5
+    },
+    'uni05': {
+        'file': 'uni05_53.ttf',
+        'size': 8,
+        'offset': -1
+    },
+    'millennium': {
+        'file': 'Pixel Millennium.ttf',
+        'size': 8,
+        'offset': 0
+    }
+}
+
+marquee_settings = {
+    'serial': None,
+    'font': fonts['tinyunicode'],
+    'delay': 0.035,
+    'space_width': 2,
+    'font_dir': 'fonts'
+}
 
 
-def random_blink():
-    from random import randint
-    while False:
-        led_output(randint(0, 255))
+class Marquee(object):
+    def __init__(self, serial=None, font=None, delay=0, space_width=3, initial_sleep=1.5, bits=8, font_dir='.'):
+        self.delay = delay
+        self.space_width = space_width
+        self.bits = bits
+        if serial is not None:
+            from serial import Serial
+            self.serial = Serial(**serial)
+            # need to wait for arduino to reset
+            sleep(initial_sleep)
+        else:
+            self.serial = None
+        if font is not None:
+            self.font_offset = font.pop('offset', 0)
+            if font['file'] is not None:
+                font['file'] = font_dir + '/' + font['file']
+            self.font = ImageFont.truetype(font['file'], font['size'], encoding='unic')
+        else:
+            self.font = None
+            self.font_offset = 0
 
+    def output(self, num):
+        # zero padded lowercase 8-bit hex
+        h = ('%02X' % (num)).lower()
+        if self.serial is not None:
+            self.serial.write(h + ' ')
+        else:
+            # write to console for debugging
+            print h, '|', bin(int(h, 16))[2:].zfill(self.bits).translate(string.maketrans('01', ' #')), '|'
+        sleep(self.delay)
 
-def iterate_binary():
-    while False:
-        for n in xrange(256):
-            led_output(n)
+    def scroll(self, w):
+        for n in xrange(w):
+            self.output(0)
 
+    def flush(self):
+        self.scroll(self.bits)
 
-def get_string_image(s, typeface):
-    font = ImageFont.truetype(typeface, 16, encoding="unic")
-    size = font.getsize(s)
-    image = Image.new("1", (size[0], 8), (0))
-    ImageDraw.Draw(image).text((0, -5), s, (1), font=font)
-    return image
+    def image_from_string(self, s):
+        size = self.font.getsize(s)
+        image = Image.new('1', (size[0], self.bits), (0))
+        ImageDraw.Draw(image).text((0, self.font_offset), s, (1), font=self.font)
+        return image
 
+    def output_image(self, image):
+        pixels = image.load()
+        for x in xrange(image.size[0]):
+            val = 0
+            for y in xrange(image.size[1]):
+                val += (1 << y) if pixels[x, y] else 0
+            self.output(val)
 
-def parse_image(image, func):
-    pixels = image.load()
-    for x in xrange(image.size[0]):
-        val = 0
-        for y in xrange(image.size[1]):
-            val += (1 << y) if pixels[x, y] else 0
-        func(val)
+    # blindly write a string without any splitting or preformatting
+    def write(self, s):
+        self.output_image(self.image_from_string(s))
 
+    # write a string with a trailing space
+    def write_word(self, w):
+        if w.__len__ > 0:
+            self.write(w)
+            self.scroll(self.space_width)
 
-# scroll past edge of display
-def led_flush():
-    led_scroll(8)
+    # write a string word by word
+    def write_line(self, s, prefix='', postfix=''):
+        s.strip()
+        if s.__len__ == 0:
+            # flush display on an empty line/string
+            self.flush()
+        else:
+            # output each word individually
+            self.write_word(prefix)
+            [self.write_word(w) for w in s.split()]
+            self.write_word(postfix)
 
+    # write random data
+    def led_random(self):
+        from random import randint
+        self.output(randint(0, 1 << self.bits - 1))
 
-def led_scroll(w):
-    for n in xrange(w):
-        led_output(0)
-
-
-def led_string(s):
-    image = get_string_image(s, font)
-    parse_image(image, led_output)
-
-
-def led_word(w):
-    led_string(w)
-    led_scroll(space_width)
+    # iterate all values to display
+    def led_interate(self):
+        for n in xrange(1 << self.bits):
+            self.output(n)
 
 
 if __name__ == "__main__":
-    for line in input():
-        line = ' '.join(line.split())
-        if line.__len__() == 0:
-            led_flush()
-        else:
-            led_word(u'\u00b6')
-            [led_word(w) for w in line.split()]
-    led_flush()
+    marquee_stdin()
